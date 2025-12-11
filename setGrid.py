2d131679a1c988dc8219dee8f31995a58b638a5b
maddiegee759 <maddie.gee.759@gmail.com>
2025-12-11 00:24:42 +0000

    saving grid as array/csv
    add_obstacle now used

--- setGrid.py
+++ setGrid.py
@@ -1,3 +1,6 @@
+#simple grid implementation for dillon to start AI
+import csv
+
 #create a 2D grid (0 = empty, 1 = obstacle)
 def create_grid(width, height):
     return [[0 for _ in range(width)] for _ in range(height)]
@@ -6,9 +9,28 @@ def create_grid(width, height):
 def add_obstacle(grid, x, y):
     grid[y][x]= 1 
 
+#display grid as 0 and 1s
 def print_grid(grid):
     for row in grid:
         print(" ".join(str(cell) for cell in row))
+
+#save coords as array
+def save_grid_as_array(grid):
+    shelves = []
+
+    for d in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[d][c] == 1:
+                shelves.append([c,d])
+    return shelves
+
+#save array as csv
+def save_grid_as_csv(shelves):
+    with open("shelves.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+
+        for x, y in shelves:
+            writer.writerow([x,y])
 
 w = 63
 h = 13
@@ -19,9 +41,7 @@ for j in range(h):
     #goes through each column
     for i in range(w):
         if i % 2 == 1:
-            grid[j][i] = 1
-
-print("first loop worked yay")
+            add_obstacle(grid, i, j)
 
 #set rows 0, 6 and 12 back to 0
 for a in range(h):
@@ -30,7 +50,10 @@ for a in range(h):
         for b in range(w):
             grid[a][b] = 0
 
-print("second?")
-
 print_grid(grid)
+print("\n")
 
+s = save_grid_as_array(grid)
+print(s)
+
+save_grid_as_csv(s)
