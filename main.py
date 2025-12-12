280aa07f92fe32f1be110fae89b3f78686f633c9
Keith Lobo <keithlobo0707@gmail.com>
2025-12-12 00:20:37 +0000

    started working on menu

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 import sqlite3
 from db_func import *
-from setGrid2 import generate_warehouse, get_warehouse_grid
+from setGrid import generate_warehouse, get_warehouse_grid
 from search import a_star_search
+
+db_connect()
+view_tables()
+print('---------------------WAREHOUSE MANAGER-----------------------\n')
+
+print("Welcome to Warehouse Manager!\n")
+
+print("Show warehouse grid\n")
+#generate warehouse
+#print warehouse grid
+
+
+print("Show robot initial state\n")
+#explain robot rest point
+
+
+
+print("Run A* (with verbose mode)\n")
+
+print("Show path + cost\n")
+
+print("Show ML model predicting path cost\n")
+
+print("Compare ML vs A*\n")
+
+'''
+•    Show warehouse grid
+•    Show robot initial state
+•    Run A* (with verbose mode)
+•    Show path + cost
+•    Show ML model predicting path cost
+•    Compare ML vs A*
+•    Mention running time
+•    Explain results clearly
+•    Clean screen layout
+•    Trim video to 5 minutes
+'''
