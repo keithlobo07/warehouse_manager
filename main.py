ca1678557d4d5e689c29f9ad932301ea74a87e5d
maddiegee759 <maddie.gee.759@gmail.com>
2025-12-12 01:06:07 +0000

    menu choices

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 import sqlite3
 from db_func import *
-from setGrid import generate_warehouse, get_warehouse_grid
+from setGrid import *
 from search import a_star_search
 
 db_connect()
-view_tables()
+#view_tables()
 print('---------------------WAREHOUSE MANAGER-----------------------\n')
 
 print("Welcome to Warehouse Manager!\n")
+print("1. Show warehouse grid\n2.Show robot initial state\n3.Run A* (with verbose mode)\n4.Show path + cost\n5.Show ML model predicting path cost\n6.Compare ML vs A*\n")
+choice = int(input("Choose\n"))
+while (choice != 0):
+    print("1. Show warehouse grid\n2.Show robot initial state\n3.Run A* (with verbose mode)\n4.Show path + cost\n5.Show ML model predicting path cost\n6.Compare ML vs A*\n0.Exit")
+    choice = int(input("Choose\n"))
 
-print("Show warehouse grid\n")
-#generate warehouse
-#print warehouse grid
-
-
-print("Show robot initial state\n")
-#explain robot rest point
-
-
-
-print("Run A* (with verbose mode)\n")
-
-print("Show path + cost\n")
-
-print("Show ML model predicting path cost\n")
-
-print("Compare ML vs A*\n")
+    match choice:
+        case 1:
+            print("Show warehouse grid selected\n")
+            #generate warehouse
+            shelf_coords, path_coords = get_warehouse_grid()
+            grid = generate_warehouse(63, 13, shelf_coords)
+            #print warehouse grid
+            print_grid(grid)
+            #should print grid be a separate case?????????????           
+        case 2:
+            print("Show robot initial state selected\n")
+            #explain robot rest point
+            #show coords of robot and status
+        case 3:
+            print("Run A* (with verbose mode) selected\n")
+            #run a_star_search
+            #return path, cost, expansions, runtime
+        case 4:
+            print("Show path + cost selected\n")
+            #can we use case 4 without case 3??????
+        case 5:
+            print("Show ML model predicting path cost selected\n")
+            #
+        case 6:
+            print("Compare ML vs A* selected\n")
+            #run both ml and a* and compare results
+        case 0:
+            print("EXITING\n")
+            exit(0)
+        case _:
+            choice = int(input("Invalid. Please choose again"))
 
 '''
 •    Show warehouse grid
